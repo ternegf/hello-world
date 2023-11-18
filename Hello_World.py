c95f01c29bb0f48fee7293f932a2c1d62200282f
ternegf <79715139+ternegf@users.noreply.github.com>
2023-11-18 13:38:24 +0000

    Hello World
    A test Hello World program

---
+++ Hello_World.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+hw_string = 'HELLO WORLD'
+hw_list = []
+hw_list = hw_string
+for i, char in enumerate(hw_list):
+    pad = (i+1)*5
+    print(f'{char: >{pad}}')
+
